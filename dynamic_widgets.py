a3d5d6543fec9735c58b46b2181d8969f994a0fd
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-07-11 20:47:09 +1000

    Add dynamic widgets demo; update squaring

---
+++ dynamic_widgets.py
@@ -0,0 +1,62 @@
+"""
+Kivy example for CP1404/CP5632, IT@JCU
+Dynamically create buttons based on content of dictionary
+Lindsay Ward
+Modified from popup_demo, 11/07/2016
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+__author__ = 'Lindsay Ward'
+
+
+class DynamicWidgetsApp(App):
+    """ Main program - Kivy app to demo dynamic widget creation """
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """
+        Construct main app
+        """
+        super(DynamicWidgetsApp, self).__init__(**kwargs)
+        # basic data example - dictionary of names: phone numbers
+        # TODO: After running it, add another entry to the dictionary and see how the layout changes
+        self.phonebook = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
+
+    def build(self):
+        """
+        Build the Kivy GUI
+        :return: reference to the root Kivy widget
+        """
+        self.title = "Dynamic Widgets"
+        self.root = Builder.load_file('dynamic_widgets.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        """
+        Create buttons from dictionary entries and add them to the GUI
+        :return: None
+        """
+        for name in self.phonebook:
+            # create a button for each phonebook entry 
+            temp_button = Button(text=name)
+            temp_button.bind(on_release=self.press_entry)
+            # add the button to the "entriesBox" using add_widget()
+            self.root.ids.entriesBox.add_widget(temp_button)
+
+    def press_entry(self, instance):
+        """
+        Handler for pressing entry buttons
+        :param instance: the Kivy button instance
+        :return: None
+        """
+        # update status text
+        name = instance.text
+        self.status_text = "{}'s number is {}".format(name, self.phonebook[name])
+
+
+DynamicWidgetsApp().run()
--- squaring.py
+++ squaring.py
@@ -1,7 +1,8 @@
 """
-CP1404 Week 11 Workshop - GUI program to square a number
+CP1404/CP5632 Practical
+Kivy GUI program to square a number
 Lindsay Ward, IT@JCU
-13/10/2015
+Started 13/10/2015
 """
 
 from kivy.app import App
@@ -20,9 +21,8 @@ class SquareNumberApp(App):
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
-    def handle_calculate(self):
+    def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
-        value = float(self.root.ids.input_number.text)
         result = value ** 2
         self.root.ids.output_label.text = str(result)
 

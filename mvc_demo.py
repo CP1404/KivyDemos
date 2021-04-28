d2c3eea73d249dbaec263ab244b7097a7c66c804
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-04-29 09:10:25 +1000

    Add docstrings and correct import for mvc_demo

--- mvc_demo.py
+++ mvc_demo.py
@@ -1,17 +1,24 @@
+"""
+Kivy example for CP1404/CP5632, IT@JCU
+This shows the use of a StringProperty object to store the "model" of MVC
+"""
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.app import StringProperty
+from kivy.properties import StringProperty
 
 
 class MVCDemo(App):
+    """The class variable in the app is the model"""
     message = StringProperty()
 
     def build(self):
+        """Construct the app"""
         self.title = "Simple MVC Demo"
         self.root = Builder.load_file('mvc.kv')
         return self.root
 
     def handle_press(self):
+        """Handle pressing the button by updating the model from the view"""
         self.message = self.root.ids.user_input.text
 
 

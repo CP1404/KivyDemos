fdad6d837b03d88300b0e9355f1108884bfa75fe
Lindsay Ward <lindsay.ward@jcu.edu.au>
2022-12-19 11:52:33 +1000

    Add background colours to dynamic_widgets

--- dynamic_widgets.py
+++ dynamic_widgets.py
@@ -1,14 +1,16 @@
 """
 Kivy example for CP1404/CP5632, IT@JCU
 Dynamically create buttons based on content of dictionary
-Lindsay Ward
-Modified from popup_demo, 11/07/2016
+Lindsay Ward, first version: 11/07/2016
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
+
+NEW_COLOUR = (1, 0, 0, 1)  # RGBA for red
+ALTERNATIVE_COLOUR = (1, 0, 1, 1)  # RGBA for magenta
 
 
 class DynamicWidgetsApp(App):
@@ -18,8 +20,7 @@ class DynamicWidgetsApp(App):
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
-        # basic data example - dictionary of names: phone numbers
-        # TODO: After running it, add another entry to the dictionary and see how the layout changes
+        # basic data (model) example - dictionary of names: phone numbers
         self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
 
     def build(self):
@@ -30,12 +31,13 @@ class DynamicWidgetsApp(App):
         return self.root
 
     def create_widgets(self):
-        """Create buttons from dictionary entries and add them to the GUI."""
+        """Create buttons from data and add them to the GUI."""
         for name in self.name_to_phone:
-            # create a button for each data entry, specifying the text and id
-            # (although text and id are the same in this case, you should see how this works)
+            # create a button for each data entry, specifying the text
             temp_button = Button(text=name)
-            temp_button.bind(on_release=self.press_entry)
+            temp_button.bind(on_press=self.press_entry)
+            # set the button's background colour
+            temp_button.background_color = NEW_COLOUR
             # add the button to the "entries_box" layout widget
             self.root.ids.entries_box.add_widget(temp_button)
 
@@ -43,8 +45,10 @@ class DynamicWidgetsApp(App):
         """Handle pressing entry buttons."""
         # get name (dictionary key) from the text of Button we clicked on
         name = instance.text
+        # change the button's background colour
+        instance.background_color = ALTERNATIVE_COLOUR
         # update status text
-        self.status_text = "{}'s number is {}".format(name, self.name_to_phone[name])
+        self.status_text = f"{name}'s number is {self.name_to_phone[name]}"
 
     def clear_all(self):
         """Clear all widgets that are children of the "entries_box" layout widget."""
